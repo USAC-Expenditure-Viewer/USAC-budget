0bd80cd561ec53fb50fb172bae402a9e24289929
tylerliu <tylersiqi@163.com>
2020-05-04 23:41:19 -0700

    Updates

---
+++ precache-manifest.8e6e18c2f03927823f575f7877e648ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1671ccfc5a9d24ec5e5dd6e03434df40",
+    "url": "/USAC-budget/index.html"
+  },
+  {
+    "revision": "943e4badb60a04f536a8",
+    "url": "/USAC-budget/static/css/2.61a570a4.chunk.css"
+  },
+  {
+    "revision": "07cbbdf3563541974a42",
+    "url": "/USAC-budget/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "943e4badb60a04f536a8",
+    "url": "/USAC-budget/static/js/2.3eddb8cd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/USAC-budget/static/js/2.3eddb8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cbbdf3563541974a42",
+    "url": "/USAC-budget/static/js/main.5ff1178c.chunk.js"
+  },
+  {
+    "revision": "fdfef08f0811d018f253",
+    "url": "/USAC-budget/static/js/runtime-main.d708de8d.js"
+  }
+]);
