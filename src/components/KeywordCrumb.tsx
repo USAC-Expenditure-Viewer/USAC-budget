365f49ae1308337f8f6bfcb596a6740340294a2b
tylerliu <tylersiqi@163.com>
2020-05-23 22:40:51 -0700

    appearance change

--- src/components/KeywordCrumb.tsx
+++ src/components/KeywordCrumb.tsx
@@ -19,16 +19,16 @@ export default class KeywordCrumb extends Component<DataLoaderProps> {
             <Breadcrumbs separator=">" style={this.props.style}>
                 <Tooltip title="Remove All Filters">
                     <Link key={-1} color="textPrimary"
-                        onClick={() => this.props.dataloader.sliceFilter(0)}>Transactions(${KMFormat(this.props.dataloader.getDatasetTotal())})</Link>
+                        onClick={() => this.props.dataloader.sliceFilter(0)}>Transactions [${KMFormat(this.props.dataloader.getDatasetTotal())}]</Link>
                 </Tooltip>
                 {list.slice(0, -1).map((filter, index) => (
                     <Tooltip title="View This Filter">
                         <Link key={index} color="textSecondary"
-                              onClick={() => this.props.dataloader.sliceFilter(index + 1)}>{filter.category}: {filter.name}(${KMFormat(filter.amount)})</Link>
+                              onClick={() => this.props.dataloader.sliceFilter(index + 1)}>{filter.category}: {filter.name} [${KMFormat(filter.amount)}]</Link>
                     </Tooltip>
                 ))}
                 {list.length > 0 ? (<Typography color="textPrimary"
-                                                key={list.length}>{list[list.length - 1].category}: {list[list.length - 1].name}(${KMFormat(list[list.length - 1].amount)})</Typography>) : null}
+                                                key={list.length}>{list[list.length - 1].category}: {list[list.length - 1].name} [${KMFormat(list[list.length - 1].amount)}]</Typography>) : null}
             </Breadcrumbs>
         )
     }
