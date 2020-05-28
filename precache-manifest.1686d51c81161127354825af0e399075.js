d0ebf7f70969f6154d5ef18c9b792cf8b40bc349
tylerliu <tylersiqi@163.com>
2020-05-27 20:57:25 -0700

    Updates

---
+++ precache-manifest.1686d51c81161127354825af0e399075.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6015e31c445112ec6cc56457994a82b1",
+    "url": "/USAC-budget/index.html"
+  },
+  {
+    "revision": "7bd7284d3570c2832aa1",
+    "url": "/USAC-budget/static/css/2.61a570a4.chunk.css"
+  },
+  {
+    "revision": "c62687cf9167875b97da",
+    "url": "/USAC-budget/static/css/main.cf2a77c1.chunk.css"
+  },
+  {
+    "revision": "7bd7284d3570c2832aa1",
+    "url": "/USAC-budget/static/js/2.a78abebb.chunk.js"
+  },
+  {
+    "revision": "13952082754fdd9789d73c2b86bf6d06",
+    "url": "/USAC-budget/static/js/2.a78abebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62687cf9167875b97da",
+    "url": "/USAC-budget/static/js/main.d01f4f86.chunk.js"
+  },
+  {
+    "revision": "fdfef08f0811d018f253",
+    "url": "/USAC-budget/static/js/runtime-main.d708de8d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/USAC-budget/precache-manifest.81e35a6aa354aef2a2ef9338e1a10b63.js"
+  "/USAC-budget/precache-manifest.1686d51c81161127354825af0e399075.js"
 );
 
 self.addEventListener('message', (event) => {
