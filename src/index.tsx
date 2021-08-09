7e7d8fc8b3cbbd3566de16af47fdca6fe50ace66
JacobLinder <jacob.linder.jl@gmail.com>
2021-08-08 21:27:16 -0700

    changing tracking id

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import $ from 'jquery';
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-EY4SW3Z3FZ"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
   <React.StrictMode>
