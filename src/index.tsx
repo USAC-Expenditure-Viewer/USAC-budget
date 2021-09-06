3f770e8f1117b9d3b2a71c4a125f7a1990766861
JacobLinder <jacob.linder.jl@gmail.com>
2021-09-06 10:41:05 -0700

    removing old analytics attempt

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ReactGA from 'react-ga';
-const TRACKING_ID = "G-EY4SW3Z3FZ"; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
